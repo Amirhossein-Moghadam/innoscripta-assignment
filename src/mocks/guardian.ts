3524125379490e675da5303b34921c2868f33f71
Amirhossein Moghadam <Amirhoussein.Moghadam79@gmail.com>
2024-08-11 01:40:27 +0330

    feat: news mock data

---
+++ src/mocks/guardian.ts
@@ -0,0 +1,162 @@
+import { Guardian } from "common/types/news-guardian.type";
+
+export const GuardianData: Guardian[] = [
+  {
+    id: "sport/live/2024/aug/10/paris-2024-olympics-day-15-live-updates-today-schedule-events-athletics-marathon-basketball-golf",
+    type: "liveblog",
+    sectionId: "sport",
+    sectionName: "Sport",
+    webPublicationDate: "2024-08-10T21:44:06Z",
+    webTitle:
+      "Paris Olympics day 15: GB take both 4x400m bronzes, Bell shines in 1500m, US win basketball gold and more – as it happened",
+    webUrl:
+      "https://www.theguardian.com/sport/live/2024/aug/10/paris-2024-olympics-day-15-live-updates-today-schedule-events-athletics-marathon-basketball-golf",
+    apiUrl:
+      "https://content.guardianapis.com/sport/live/2024/aug/10/paris-2024-olympics-day-15-live-updates-today-schedule-events-athletics-marathon-basketball-golf",
+    isHosted: false,
+    pillarId: "pillar/sport",
+    pillarName: "Sport",
+  },
+  {
+    id: "sport/article/2024/aug/10/usa-france-mens-basketball-final-olympics-paris-2024-result",
+    type: "article",
+    sectionId: "sport",
+    sectionName: "Sport",
+    webPublicationDate: "2024-08-10T21:33:36Z",
+    webTitle:
+      "‘Avalanche came’: Curry dazzles late as US beat France for Olympic basketball gold",
+    webUrl:
+      "https://www.theguardian.com/sport/article/2024/aug/10/usa-france-mens-basketball-final-olympics-paris-2024-result",
+    apiUrl:
+      "https://content.guardianapis.com/sport/article/2024/aug/10/usa-france-mens-basketball-final-olympics-paris-2024-result",
+    isHosted: false,
+    pillarId: "pillar/sport",
+    pillarName: "Sport",
+  },
+  {
+    id: "sport/article/2024/aug/10/gutted-but-proud-caden-cunningham-kicks-his-way-to-taekwondo-silver",
+    type: "article",
+    sectionId: "sport",
+    sectionName: "Sport",
+    webPublicationDate: "2024-08-10T21:33:06Z",
+    webTitle:
+      "‘Gutted but proud’ Caden Cunningham kicks his way to taekwondo silver",
+    webUrl:
+      "https://www.theguardian.com/sport/article/2024/aug/10/gutted-but-proud-caden-cunningham-kicks-his-way-to-taekwondo-silver",
+    apiUrl:
+      "https://content.guardianapis.com/sport/article/2024/aug/10/gutted-but-proud-caden-cunningham-kicks-his-way-to-taekwondo-silver",
+    isHosted: false,
+    pillarId: "pillar/sport",
+    pillarName: "Sport",
+  },
+  {
+    id: "us-news/article/2024/aug/10/trump-campaign-claims-hacked",
+    type: "article",
+    sectionId: "us-news",
+    sectionName: "US news",
+    webPublicationDate: "2024-08-10T21:33:04Z",
+    webTitle: "Donald Trump 2024 campaign claims emails were hacked",
+    webUrl:
+      "https://www.theguardian.com/us-news/article/2024/aug/10/trump-campaign-claims-hacked",
+    apiUrl:
+      "https://content.guardianapis.com/us-news/article/2024/aug/10/trump-campaign-claims-hacked",
+    isHosted: false,
+    pillarId: "pillar/news",
+    pillarName: "News",
+  },
+  {
+    id: "sport/article/2024/aug/10/georgia-bell-leads-rush-of-silvers-and-bronzes-as-team-gb-take-medal-tally-race-to-the-wire",
+    type: "article",
+    sectionId: "sport",
+    sectionName: "Sport",
+    webPublicationDate: "2024-08-10T21:32:11Z",
+    webTitle:
+      "Georgia Bell leads rush of silvers and bronzes as Team GB take medal tally race to the wire ",
+    webUrl:
+      "https://www.theguardian.com/sport/article/2024/aug/10/georgia-bell-leads-rush-of-silvers-and-bronzes-as-team-gb-take-medal-tally-race-to-the-wire",
+    apiUrl:
+      "https://content.guardianapis.com/sport/article/2024/aug/10/georgia-bell-leads-rush-of-silvers-and-bronzes-as-team-gb-take-medal-tally-race-to-the-wire",
+    isHosted: false,
+    pillarId: "pillar/sport",
+    pillarName: "Sport",
+  },
+  {
+    id: "sport/live/2024/aug/10/usa-v-france-olympic-mens-basketball-final-live-updates",
+    type: "liveblog",
+    sectionId: "sport",
+    sectionName: "Sport",
+    webPublicationDate: "2024-08-10T21:30:44Z",
+    webTitle:
+      "Late Curry dagger lifts US over France to seal fifth straight Olympic gold – as it happened",
+    webUrl:
+      "https://www.theguardian.com/sport/live/2024/aug/10/usa-v-france-olympic-mens-basketball-final-live-updates",
+    apiUrl:
+      "https://content.guardianapis.com/sport/live/2024/aug/10/usa-v-france-olympic-mens-basketball-final-live-updates",
+    isHosted: false,
+    pillarId: "pillar/sport",
+    pillarName: "Sport",
+  },
+  {
+    id: "sport/article/2024/aug/10/hamish-kerr-wins-dramatic-high-jump-gold-after-sudden-death-round",
+    type: "article",
+    sectionId: "sport",
+    sectionName: "Sport",
+    webPublicationDate: "2024-08-10T21:23:28Z",
+    webTitle:
+      "Hamish Kerr wins dramatic high jump gold after sudden death round",
+    webUrl:
+      "https://www.theguardian.com/sport/article/2024/aug/10/hamish-kerr-wins-dramatic-high-jump-gold-after-sudden-death-round",
+    apiUrl:
+      "https://content.guardianapis.com/sport/article/2024/aug/10/hamish-kerr-wins-dramatic-high-jump-gold-after-sudden-death-round",
+    isHosted: false,
+    pillarId: "pillar/sport",
+    pillarName: "Sport",
+  },
+  {
+    id: "sport/article/2024/aug/10/georgia-bell-1500m-final-olympic-games-paris-2024",
+    type: "article",
+    sectionId: "sport",
+    sectionName: "Sport",
+    webPublicationDate: "2024-08-10T21:15:57Z",
+    webTitle:
+      "Georgia Bell completes epic journey from parkruns to 1500m Olympic bronze",
+    webUrl:
+      "https://www.theguardian.com/sport/article/2024/aug/10/georgia-bell-1500m-final-olympic-games-paris-2024",
+    apiUrl:
+      "https://content.guardianapis.com/sport/article/2024/aug/10/georgia-bell-1500m-final-olympic-games-paris-2024",
+    isHosted: false,
+    pillarId: "pillar/sport",
+    pillarName: "Sport",
+  },
+  {
+    id: "sport/article/2024/aug/10/team-gb-win-relay-bronze-but-powerless-to-stop-usa-and-benjamin",
+    type: "article",
+    sectionId: "sport",
+    sectionName: "Sport",
+    webPublicationDate: "2024-08-10T21:08:46Z",
+    webTitle: "Team GB win relay bronze but powerless to stop USA and Benjamin",
+    webUrl:
+      "https://www.theguardian.com/sport/article/2024/aug/10/team-gb-win-relay-bronze-but-powerless-to-stop-usa-and-benjamin",
+    apiUrl:
+      "https://content.guardianapis.com/sport/article/2024/aug/10/team-gb-win-relay-bronze-but-powerless-to-stop-usa-and-benjamin",
+    isHosted: false,
+    pillarId: "pillar/sport",
+    pillarName: "Sport",
+  },
+  {
+    id: "football/article/2024/aug/10/wrexham-wycombe-league-one-ryan-reynolds-phil-parkinson",
+    type: "article",
+    sectionId: "football",
+    sectionName: "Football",
+    webPublicationDate: "2024-08-10T20:23:46Z",
+    webTitle:
+      "Fans marvel at Wrexham’s rise as League One campaign begins in style",
+    webUrl:
+      "https://www.theguardian.com/football/article/2024/aug/10/wrexham-wycombe-league-one-ryan-reynolds-phil-parkinson",
+    apiUrl:
+      "https://content.guardianapis.com/football/article/2024/aug/10/wrexham-wycombe-league-one-ryan-reynolds-phil-parkinson",
+    isHosted: false,
+    pillarId: "pillar/sport",
+    pillarName: "Sport",
+  },
+];
